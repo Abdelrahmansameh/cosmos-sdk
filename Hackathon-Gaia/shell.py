dff9eb24132facb76ae11eaa95f68937c29d9662
kalari499 <kalar499@gmail.com>
2019-04-14 14:34:12 +0200

    Change gaia-genesis

--- Hackathon-Gaia/shell.py
+++ Hackathon-Gaia/shell.py
@@ -56,7 +56,7 @@ def main():
         elif line in ["testnet start", "testnet init"]:
             testnet_start()
         elif line == "clean":
-            execute("rm -f $HOME/.gaiad/config/addrbook.json $HOME/.gaiad/config/genesis.json $HOME/.gaiad/config/gent*.json")
+            execute("rm -rf $HOME/.gaiad/config/addrbook.json $HOME/.gaiad/config/genesis.json $HOME/.gaiad/config/gentx")
             execute("gaiad unsafe-reset-all")
         else:
             print("Invalid command")
